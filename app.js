4aa4e3f23bab138c0e741dd1f37f2ab79519f612
Rome Bell <romebell@Romes-MacBook-Pro.local>
2020-07-16 18:31:55 -0400

    [callback] Add calculator function

--- app.js
+++ app.js
@@ -15,3 +15,49 @@ function anotherGreeting() {
 
 let myName = 'Rome';
 console.log(greeting(myName, anotherGreeting));
+
+// Exercise
+function printArray(array, callback) {
+    for (let i = 0; i < array.length; i++) {
+        let number = array[i]; // 1, 2, ....
+
+        console.log(number); // 1
+    }
+
+    callback(array);
+}
+
+function printArrayAgain(array) {
+    for (let i = 0; i < array.length; i++) {
+        let number = array[i]; // 1, 2 ...
+
+        console.log(number); // 1, 2 
+    }
+}
+
+const numbersArray = [1,2,3,4,5,9,8,7,6];
+
+printArray(numbersArray, printArrayAgain);
+
+// Exercise 
+function calculator(num1, num2, callbackOne, callbackTwo) {
+    let result = callbackOne(num1, num2) + callbackTwo(num1, num2);
+    // 82 + -72 = 10
+
+    return result;
+}
+
+function addNumbers(num1, num2) {
+    let result = num1 + num2;
+    
+    return result;
+
+}
+
+function subtractNumbers(num1, num2) {
+    let result = num1 - num2;
+    
+    return result;
+}
+
+console.log(calculator(5,77, addNumbers, subtractNumbers));
